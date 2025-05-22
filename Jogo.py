966119d5355132d055dec3d797684547d8edb543
Matheus Mikio <matheusmikio15@gmail.com>
2025-05-22 11:28:06 -0300

    Add Tratativas

--- Jogo.py
+++ Jogo.py
@@ -19,41 +19,45 @@ VampirosPadrao = set(
         Vampiro("3")
     ]
 )
-
+valdrak = Valdrak()
 heroisCriados = set()
 
 while True:
-    print("1 - Jogar")
-    print("2 - Criar heroi")
-    print("3 - Sair")
-    opcao = int(input("Escolha: "))
-    match opcao:
-        case 1:
-            if len(heroisCriados) < 4:
-                print("Nao ha herois suficientes, dseja usar os herois padroes?")
-                opcao = input("S/N")
-                if opcao == "S":
-                    print("Iniciando o jogo...")
-                    #Usar heroisPadrao
+    try:
+        print("1 - Jogar")
+        print("2 - Criar heroi")
+        print("3 - Sair")
+        opcao = int(input("Escolha: "))
+        match opcao:
+            case 1:
+                if len(heroisCriados) < 4:
+                    print("Nao ha herois suficientes(É necessário 4 personagens), dseja usar os herois padroes?")
+                    opcao = input("S/N: ")[0].upper()
+                    if opcao == "S":
+                        print("Iniciando o jogo...")
+                        #Usar heroisPadrao
 
-                elif opcao == "N":
-                    print("Voltando")
+                    elif opcao == "N":
+                        print("Voltando...")
+                    else:
+                        print("Opcao invalida")
+            case 2:
+                if len(heroisCriados) >= 4:
+                    print("Voce ja criou todos os herois possiveis")
                     break
-        case 2:
-            if len(heroisCriados) >= 4:
-                print("Voce ja criou todos os herois possiveis")
+                nome = input("Nome: ")
+                heroisCriados.add(Personagem(nome, habilidades))
+                print(f"Heroi {nome} criado com sucesso")
+                print("Herois criados: ")
+                for heroi in heroisCriados:
+                    print(heroi.nome, end="  ")
+                print()
+                
+            case 3:
+                print("Saindo...")
                 break
-            nome = input("Nome: ")
-            heroisCriados.add(Personagem(nome, habilidades))
-            print(f"Heroi {nome} criado com sucesso")
-            print("Herois criados: ")
-            for heroi in heroisCriados:
-                print(heroi.nome, end="  ")
-            print()
-            
-        case 3:
-            print("Saindo...")
-            break
 
-        case _:
-            print("Opcao invalida")
+            case _:
+                print("Opcao invalida")
+    except ValueError:
+        print("Opcao invalida")
