3f5529049fa6f66c60d4ae76834ada6bca1d615a
Matheus Mikio <matheusmikio15@gmail.com>
2025-05-22 22:03:28 -0300

    Fix class Personagem

--- Personagens/Heroi.py
+++ Personagens/Heroi.py
@@ -1,28 +1,38 @@
 #Classe base para os heróis do jogo.
+import random
+from Habilidades import habilidadesHeroi
+
 class Personagem:
     def __init__(self, nome : str, habilidades : list):
         self.nome = nome
         self.vida = 100
         self.energia = 1
+        self.habilidadesPersonagem = habilidades
         self.habilidades = []
         self.mao = []
-        for habilidade in habilidades:
+
+        #Criando uma lista de habilidades com 5 cópias de cada habilidade.
+        for habilidade in self.habilidadesPersonagem:
             self.habilidades.extend(
                 [habilidade.copy() for _ in range(5)]
             )
+        #Embaralha as cartas e distribui a mão inicial
+        self.embaralhar()
+        
+    def embaralhar(self):
+        random.shuffle(self.habilidades)
         self.pegarCartas(5)
 
+    #Adiciona cartas do baralho para a mão do personagem.
     def pegarCartas(self, quantidade : int = 1):
         if len(self.mao) >= 5:
             print("Você não pode pegar mais cartas (Maximo de 5 cartas na mão).")
             return
 
-        for i in range(quantidade):
+        for _ in range(quantidade):
             if len(self.habilidades) == 0:
                 print("Você não possui mais cartas disponíveis")
                 return
-
-            random.shuffle(self.habilidades) 
             self.mao.append(self.habilidades.pop())
 
     def atacar(self, alvo, numeroHabilidade : int): 
@@ -33,7 +43,8 @@ class Personagem:
         if numeroHabilidade < 1 or numeroHabilidade > len(self.mao):
             print("Número de carta inválido.")
             return
-        
+
+        # Executa o ataque se tiver energia suficiente
         if self.energia >= self.mao[numeroHabilidade-1]["custo"]:
             alvo.vida -= self.mao[numeroHabilidade-1]["dano"]
             self.energia -= self.mao[numeroHabilidade-1]["custo"]
@@ -50,9 +61,14 @@ class Personagem:
         for index, carta in enumerate(self.mao):
             print(f"{index + 1} - {carta['nome']}: Dano: {carta['dano']}, Custo: {carta['custo']}")
 
+    #Usado para resetar o personagem depois de finalizar o combate.
     def resetarPersonagem(self):
         self.vida = 100
         self.energia = 1
         self.mao = []
-        random.shuffle(self.habilidades) 
-        self.pegarCartas(5)
+        self.habilidades = []
+        for habilidade in self.habilidadesPersonagem:
+            self.habilidades.extend(
+                [habilidade.copy() for _ in range(5)]
+            )
+        self.embaralhar()
